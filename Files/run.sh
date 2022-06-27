44b4e623fa8dade9c3280d87a4811dff134e5a58
Poorya Abbasi <me@pooryaa.com>
2022-06-27 23:42:35 +0430

    Ignored and removed files

--- Files/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-./jflex/bin/jflex Lexer.flex
-sed -i .old 's/public class Lexer implements sym, java_cup.runtime.Scanner {/public class Lexer extends sym implements java_cup.runtime.Scanner {/g' Lexer.java
-java -jar ./java-cup-11b.jar -parser "Parser" ./Parser.cup
-javac -cp .:java-cup-11b.jar Main.java
-java -cp .:java-cup-11b-runtime.jar Main Program.java
